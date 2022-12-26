97b8579101ac0da04a497a95fc1ce13f8eed9e87
SAURAV KUMAR JHA <mysauravnitw2019@outlook.com>
2022-12-26 12:05:51 +0530

    link to frontend removed

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send(
-    'welcome! This is backend of Todo App which can be found <a href="https://todoapp2023.herokuapp.com/">here</a>.'
-  );
+    res.send(
+        'welcome! This is backend of Todo App which can be found <a href="/">here</a>.'
+    );
 });
 
 module.exports = router;
